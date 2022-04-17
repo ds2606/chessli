16725ff00f93459b9834760423729f17fe9ca740
Daniel Schreck <daniel.s.schreck@gmail.com>
2022-04-16 21:38:19 -0400

    Clean backend variable naming, add ua-header

--- chessli_lib.py
+++ chessli_lib.py
@@ -6,22 +6,25 @@ import requests
 
 def get_lastgame_json(player):
     # ensure valid username
-    if not re.compile("^[\w\-]{3,25}$").match(player):
+    if not re.compile('^[\w\-]{3,25}$').match(player):
         return json.dumps({'error': 'invalid username'})
 
-    archiveAPI = "https://api.chess.com/pub/player/" + player + "/games/archives"
+    # get JSON of most recent played game
+    archivesAPI = 'https://api.chess.com/pub/player/' + player + '/games/archives'
+    ua_header = {'user-agent': 'chessli/0.0.1, daniel.s.schreck@gmail.com'}
     try:
-        recentGamesAPI = requests.get(archiveAPI).json()['archives'][-1]
+        gamesAPI = requests.get(archivesAPI, headers=ua_header).json()['archives'][-1]
     except KeyError:
         return json.dumps({'error': 'user ' + player + ' not found'})
     except IndexError:
         return json.dumps({'error': player + ' has played no games'})
+    game = requests.get(gamesAPI).json()['games'][-1]
 
-    game = requests.get(recentGamesAPI).json()['games'][-1]
-
+    # import game to Lichess and get URL
     importAPI = "https://lichess.org/api/import"
     gameURL = requests.post(importAPI, data={'pgn': game['pgn']}).json()['url']
 
+    # return payload to frontend
     payload = {
         'white': game['white']['username'],
         'black': game['black']['username'],
@@ -30,6 +33,7 @@ def get_lastgame_json(player):
         'link': gameURL
     }
     return json.dumps(payload)
+
 
 # deprecated, just used for testing (gets lichess URL for chess.com user)
 def get_lichess_url_bash(player):
